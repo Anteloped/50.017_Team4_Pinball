2f5a41e14a6d3fa269898ca32070871b0a309d96
sihvn <sihan0303@gmail.com>
2023-03-20 23:52:57 +0800

    restart + more obstacles + textures

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Ball : MonoBehaviour
 {
     Rigidbody rb;
+    public Vector3 endGameCoord ;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
@@ -15,6 +17,10 @@ public class Ball : MonoBehaviour
     {
         if (rb.velocity.y > 0) {
             rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        }
+
+        if ((rb.position.z-endGameCoord.z)<=0){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
