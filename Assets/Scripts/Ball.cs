e03cee45321ce724152bfc5ef844040437a4a3c2
jaron <jaronho@protonmail.com>
2023-03-21 16:07:50 +0800

    Added invisible ceiling, fixed flippers

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -15,9 +15,9 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (rb.velocity.y > 0) {
-            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-        }
+        //if (rb.velocity.y > 0) {
+        //    rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        //}
 
         if ((rb.position.z-endGameCoord.z)<=0){
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
--- Assets/Scripts/Flipper.cs
+++ Assets/Scripts/Flipper.cs
@@ -8,12 +8,14 @@ public class Flipper : MonoBehaviour
     [SerializeField] float maxAngle = 20.0f;
     [SerializeField] float flipTime = 1.0f;
     [SerializeField] string buttonName = "Fire1";
+    [SerializeField] bool isLeftSide = false;
     Quaternion initialOrientation;
+    Vector3 initialAngles;
     
     // Start is called before the first frame update
     void Start()
     {
-        initialOrientation = transform.rotation;
+        initialAngles = transform.rotation.eulerAngles;
     }
 
     // Update is called once per frame
@@ -23,12 +25,23 @@ public class Flipper : MonoBehaviour
         {
             //Rotate the flipper
             transform.Rotate(Vector3.up * maxAngle / flipTime * Time.deltaTime);
-            Vector3 angles = initialOrientation.eulerAngles;
-            if(transform.rotation.eulerAngles.y - angles.y >= maxAngle)
+            Vector3 angles = transform.rotation.eulerAngles;
+            if (!isLeftSide && angles.y - initialAngles.y >= maxAngle)
             {
-                Quaternion newOrientation = transform.rotation;
-                newOrientation.eulerAngles = new Vector3(angles.x, angles.y + maxAngle, angles.z);
-                transform.rotation = newOrientation;
+                transform.rotation = Quaternion.Euler(initialAngles + Vector3.up * maxAngle);
+            }
+            else if (isLeftSide && angles.y - initialAngles.y <= 360 + maxAngle) {
+                transform.rotation = Quaternion.Euler(initialAngles + Vector3.up * (360 + maxAngle));
+            }
+        }
+        else if (transform.rotation.eulerAngles.y != initialAngles.y) {
+            transform.Rotate(Vector3.up * -maxAngle / flipTime * Time.deltaTime);
+            Vector3 angles = transform.rotation.eulerAngles;
+            if (isLeftSide && angles.y <= -maxAngle) {
+                transform.rotation = Quaternion.Euler(initialAngles);
+            }
+            else if (!isLeftSide && angles.y >= 360 - maxAngle) {
+                transform.rotation = Quaternion.Euler(initialAngles);
             }
         }
     }
