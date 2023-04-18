018d1f5b6954c8c8bdf8e7c7b80422ecbb5a7730
eharnlee <eharnlee@gmail.com>
2023-04-18 20:06:50 +0800

    Update Parameters.cs
    Allow the players to change parameters by interacting with sliders in the game UI. Change Bumper Power, Ball Bounciness and Gravity

--- Assets/Scripts/Parameters.cs
+++ Assets/Scripts/Parameters.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Parameters : MonoBehaviour
 {
-    private float bumperPower;
+    private float bumperPower; // default 1.8f
+    private float ballBounciness; // default 0.55f
+    private float gravity; // default -10f
 
     // Start is called before the first frame update
     void Start()
     {
         // initialize values for the parameters
-        bumperPower = 2.0f;
+        bumperPower = 2f;
+        gravity = -10f;
     }
 
     // Update is called once per frame
@@ -24,8 +27,36 @@ public class Parameters : MonoBehaviour
         return this.bumperPower;
     }
 
-    public void setBumperPower(float power)
+    public float getBallBounciness()
     {
-        this.bumperPower = power;
+        return this.ballBounciness;
+    }
+
+    public float getGravity()
+    {
+        return this.gravity;
+    }
+
+    public void setBumperPower(float powerLevel)
+    {
+        this.bumperPower = powerLevel / 5f * 3f;
+        GameObject.Find("PlayArea").GetComponent<NarrowPhase>().setBumperPower(this.bumperPower);
+
+        Debug.Log(string.Format("Bumper Power changed to {0}", bumperPower));
+    }
+
+    public void setBallBounciness(float bouncinessLevel)
+    {
+        this.ballBounciness = (bouncinessLevel - 3f) / 10 + 0.55f;
+        GameObject.Find("PlayArea").GetComponent<NarrowPhase>().setBallBounciness(this.ballBounciness);
+
+        Debug.Log(string.Format("Ball Bounciness changed to {0}", ballBounciness));
+    }
+
+    public void setGravity(float gravityLevel)
+    {
+        this.gravity = - (gravityLevel - 3f) * 2.5f - 10f;
+        GameObject.FindGameObjectWithTag("Ball").GetComponent<Ball>().setGravity(this.gravity);
+        Debug.Log(string.Format("Gravity changed to {0}", gravity));
     }
 }
