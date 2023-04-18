cb4406813fa2ca0a864f4ed7242a9b7a30f83215
eharnlee <eharnlee@gmail.com>
2023-04-18 14:40:05 +0800

    Create Parameters.cs

---
+++ Assets/Scripts/Parameters.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parameters : MonoBehaviour
+{
+    private float bumperPower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // initialize values for the parameters
+        bumperPower = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float getBumperPower()
+    {
+        return this.bumperPower;
+    }
+
+    public void setBumperPower(float power)
+    {
+        this.bumperPower = power;
+    }
+}
