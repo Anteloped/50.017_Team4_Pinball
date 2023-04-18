76c5cb5dbb44c1da3f307b08f4309781b9fe02de
sihvn <sihan0303@gmail.com>
2023-04-18 15:54:59 +0800

    BoundingBox

--- Assets/Scripts/BoundingBox.cs
+++ Assets/Scripts/BoundingBox.cs
@@ -12,93 +12,83 @@ public class BoundingBox : MonoBehaviour
 
     private List<GameObject> objects;
     private List<List<GameObject>> box_obj;
-    private List<BoundingBox> Boxes;
-    public Vector3 min;
-    public Vector3 max;
-
+    private List<GameObject> boxes;
+    public GameObject boundingBox1;
+    public GameObject boundingBox2;
+    public GameObject boundingBox3;
+    public GameObject boundingBox4;
     NarrowPhase collision_check;
-
-
-    public BoundingBox(Vector2 min, Vector2 max)
-    {
-        this.min = min;
-        this.max = max;
-    }
+    
     void Start()
     {
-        Debug.Log("Bounding Box");
-
         table = GameObject.FindGameObjectWithTag("Table");
-        box = new BoundingBox(new Vector3(), new Vector3());
-
+        //box = table.AddComponent<BoundingBox>();
         objects = GetAllChildren(table);
-        Boxes = new List<BoundingBox>();
-        //box.Split(Boxes);
-
-        //Debug.Log("Box count");
-        //Debug.Log(Boxes.Count);
-
-        //objInBox(objects, Boxes);
-
-        collision_check = table.AddComponent<NarrowPhase>();
+        boxes = new List<GameObject>();
+        boxes.Add(boundingBox1);
+        boxes.Add(boundingBox2);
+        boxes.Add(boundingBox3);
+        boxes.Add(boundingBox4);
+        
+        box_obj=objInBox(objects, boxes);
+        collision_check = table.GetComponent<NarrowPhase>();
     }
-    //collision_check.collide(obj,ball,ballScript);
     // Update is called once per frame
     void Update()
     {
-        //ball = GameObject.FindGameObjectWithTag("Ball");
-        //ballScript = ball.GetComponent<Ball>();
+        ball = GameObject.FindGameObjectWithTag("Ball");
+        ballScript = ball.GetComponent<Ball>();
 
-        //for (int i = 0; i <= 3; i++)
-        //{
-        //    if (ballInsideBox(ballScript, Boxes[i]))
-        //    {
-        //        foreach (GameObject obj in box_obj[i])
-        //        {
-        //            collision_check.collide(obj);
-        //        }
-        //    }
-        //}
+        for (int i = 0; i <= 3; i++)
+        {
+           if (ballInsideBox(ballScript, boxes[i]))
+           {
+               foreach (GameObject obj in box_obj[i])
+               {
+                   collision_check.collide(obj);
+               }
+           }
+        }
     }
 
-    public void objInBox(List<GameObject> objects, List<BoundingBox> Boxes)
+    public List<List<GameObject>> objInBox(List<GameObject> objects, List<GameObject> boxes)
     {
+        List<List<GameObject>> box_obj = new List<List<GameObject>>();
         foreach (GameObject obj in objects)
         {
             for (int i = 0; i <= 3; i++)
             {
-                if (isInsideBox(obj, Boxes[i]))
+                box_obj.Add(new List<GameObject>());
+                if (isInsideBox(obj, boxes[i]))
                 {
                     box_obj[i].Add(obj);
                 }
-                Debug.Log(box_obj[i]);
-                Debug.Log(box_obj);
             }
         }
+        return box_obj;
     }
 
-    public bool ballInsideBox(Ball ball, BoundingBox b)
+    public bool ballInsideBox(Ball ball, GameObject b)
     {
-        if (ball.transform.position.x >= b.min.x && ball.transform.position.x >= b.max.x &&
-        ball.transform.position.y >= b.min.y && ball.transform.position.y >= b.max.y &&
-        ball.transform.position.z >= b.min.z && ball.transform.position.z >= b.max.z)
+        MeshRenderer renderer = b.GetComponent<MeshRenderer>();
+        if (ball.transform.position.x >= renderer.bounds.min.x && ball.transform.position.x >=renderer.bounds.max.x &&
+        ball.transform.position.y >= renderer.bounds.min.y && ball.transform.position.y >= renderer.bounds.max.y &&
+        ball.transform.position.z >= renderer.bounds.min.z && ball.transform.position.z >=renderer.bounds.max.z)
         {
             return true;
         }
         return false;
     }
-    public bool isInsideBox(GameObject o, BoundingBox b)
+    public bool isInsideBox(GameObject o, GameObject b)
     {
-        if (o.transform.position.x >= b.min.x && o.transform.position.x >= b.max.x &&
-        o.transform.position.y >= b.min.y && o.transform.position.y >= b.max.y &&
-        o.transform.position.z >= b.min.z && o.transform.position.z >= b.max.z)
+        MeshRenderer renderer = b.GetComponent<MeshRenderer>();
+        if (o.transform.position.x >= renderer.bounds.min.x && o.transform.position.x >=renderer.bounds.max.x &&
+        o.transform.position.y >= renderer.bounds.min.y && o.transform.position.y >= renderer.bounds.max.y &&
+        o.transform.position.z >= renderer.bounds.min.z && o.transform.position.z >=renderer.bounds.max.z)
         {
             return true;
         }
-        else
-        {
-            return false;
-        }
+        return false;
     }
 
     public List<GameObject> GetAllChildren(GameObject parent)
@@ -106,7 +96,6 @@ public class BoundingBox : MonoBehaviour
         List<GameObject> objects = new List<GameObject>();
         for (int i = 0; i < parent.transform.childCount; i++)
         {
-
             Transform childTransform = parent.transform.GetChild(i);
             GameObject childGameObject = childTransform.gameObject;
             objects.Add(childGameObject);
@@ -115,34 +104,5 @@ public class BoundingBox : MonoBehaviour
         }
         return objects;
     }
-    public void Split(List<BoundingBox> result)
-    {
-        GameObject Bound = GameObject.FindGameObjectWithTag("BoundingBox");
-        MeshRenderer renderer = Bound.GetComponent<MeshRenderer>();
-
-        // Calculate the bounding box in world space
-        min = renderer.bounds.min;
-        max = renderer.bounds.max;
-
-        float midX = (min.x + max.x) / 2;
-        Vector3 centerLeft = new Vector3(min.x + midX, 0, (max.z + min.z) / 2);
-        Vector3 centerRight = new Vector3(max.x - midX, 0, (max.z + min.z) / 2);
-        BoundingBox leftBottom = Bound.AddComponent<BoundingBox>();
-        leftBottom.min = min;
-        leftBottom.max = centerLeft;
-        BoundingBox rightBottom = Bound.AddComponent<BoundingBox>();
-        rightBottom.min = new Vector3(centerLeft.x, 0, min.z);
-        rightBottom.max = new Vector3(centerRight.x, 0, centerLeft.z);
-        BoundingBox leftTop = Bound.AddComponent<BoundingBox>();
-        leftTop.min = new Vector3(min.x, 0, centerLeft.z);
-        leftTop.max = new Vector3(centerLeft.x, 0, max.z);
-        BoundingBox rightTop = Bound.AddComponent<BoundingBox>();
-        rightTop.min = centerRight;
-        rightTop.max = max;
-
-        result.Add(leftTop);
-        result.Add(rightTop);
-        result.Add(leftBottom);
-        result.Add(rightBottom);
-    }
+   
 }
